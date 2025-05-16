7514317749ddf907d4896e2b248c27b5ed09a2dc
SSSycamore <2083551494@qq.com>
2025-05-17 00:36:12 +0800

    添加User管理员身份标识和age、phone属性

--- ovp-pojo/src/main/java/com/ovp/entity/User.java
+++ ovp-pojo/src/main/java/com/ovp/entity/User.java
@@ -20,6 +20,10 @@ public class User {
     private String password; //密码
     private String email; //邮箱
     private String avatarUrl; //头像url
+    private Integer age; //年龄
+    private String phone; //手机号
+    private Integer status; //用户状态 0-正常 1-封禁
+    private String role; //用户角色 0-普通用户 1-管理员
     private String nickname; //昵称
     private String gender; //性别
     private String bio; //个性签名
